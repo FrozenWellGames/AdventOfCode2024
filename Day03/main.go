823ce4b031961741c8cef56eab1ed7b6ccf54eef
RichardColdwell <richard.coldwell@codewizards.co.uk>
2024-12-03 15:31:25 +0000

    Update main.go

--- Day03/main.go
+++ Day03/main.go
@@ -126,6 +126,16 @@ func calculateTotalPartOne(numbers [][]int) int {
 	return returnTotal
 }
 
+// another way of getting input
+func readInput() string {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return ""
+	}
+	return string(data)
+}
+
 func main() {
 	fmt.Println("Advent Of Code 2024 - Day 3")
 	fmt.Println("Part 1")
