7d102364dc4a6f202f2308c4892b2d785916997c
FrozenWellGames <frozenwellgames@gmail.com>
2024-12-04 13:32:00 +0000

    Day 1 added

--- Day01/main.go
+++ Day01/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
@@ -19,7 +23,6 @@ func ReadFile(filepath string) []string {
 	defer file.Close()
 
 	var returnInput []string
-	// Reading from a file using scanner.
 	scanner := bufio.NewScanner(file)
 	var line string
 	var cnt = 1
@@ -31,9 +34,64 @@ func ReadFile(filepath string) []string {
 	return returnInput
 }
 
+func convertStringToInt(s string) int {
+	returnInt, err := strconv.Atoi(s)
+
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return 0
+	}
+	return returnInt
+}
+
+func CreateData(input []string) ([]int, []int) {
+
+	var returnArrayOne []int
+	var returnArrayTwo []int
+	for i := 0; i < len(input); i++ {
+
+		var stringArray = strings.Fields(input[i])
+
+		returnArrayOne = append(returnArrayOne, convertStringToInt(stringArray[0]))
+		returnArrayTwo = append(returnArrayTwo, convertStringToInt(stringArray[1]))
+
+	}
+
+	sort.Ints(returnArrayOne[:])
+	sort.Ints(returnArrayTwo[:])
+
+	return returnArrayOne, returnArrayTwo
+}
+
+func part1(arrayOne []int, arrayTwo []int) int {
+	var totalPartOne int
+	for i := 0; i < len(arrayOne); i++ {
+		totalPartOne += int(math.Abs(float64(arrayOne[i]) - float64(arrayTwo[i])))
+	}
+	return totalPartOne
+}
+
+func part2(arrayOne []int, arrayTwo []int) int {
+	var totalPartTwo int
+	for x := 0; x < len(arrayOne); x++ {
+		var currentArrayOneValue = arrayOne[x]
+		var countInArrayTwo = 0
+
+		for y := 0; y < len(arrayTwo); y++ {
+			if currentArrayOneValue == arrayTwo[y] {
+				countInArrayTwo++
+			}
+		}
+		totalPartTwo += currentArrayOneValue * countInArrayTwo
+	}
+	return totalPartTwo
+}
+
 func main() {
 	fmt.Println("Advent Of Code 2024 - Day 1")
-	var input = ReadFile("./test.txt")
-	fmt.Println(input[0])
+	var input = ReadFile("./input.txt")
+	arrayOne, arrayTwo := CreateData(input)
+	fmt.Println("Part 1 = ", part1(arrayOne, arrayTwo))
+	fmt.Println("Part 2 = ", part2(arrayOne, arrayTwo))
 
 }
