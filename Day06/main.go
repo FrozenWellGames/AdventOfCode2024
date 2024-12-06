367887c464d5b4ae7d6459f2899e2b8e9e826adb
FrozenWellGames <frozenwellgames@gmail.com>
2024-12-06 10:26:14 +0000

    Day 6 part 2

--- Day06/main.go
+++ Day06/main.go
@@ -59,7 +59,7 @@ func checkUp(inputMap [][]string, currentPosition []int) ([]int, bool) {
 	if inputMap[currentPosition[0]-1][currentPosition[1]] == "." || inputMap[currentPosition[0]-1][currentPosition[1]] == "x" {
 		newPosition = append(newPosition, currentPosition[0]-1)
 		newPosition = append(newPosition, currentPosition[1])
-	} else if inputMap[currentPosition[0]-1][currentPosition[1]] == "#" {
+	} else if inputMap[currentPosition[0]-1][currentPosition[1]] == "#" || inputMap[currentPosition[0]-1][currentPosition[1]] == "o" {
 		newPosition = append(newPosition, currentPosition[0])
 		newPosition = append(newPosition, currentPosition[1])
 		changeDir = true
@@ -73,7 +73,7 @@ func checkRight(inputMap [][]string, currentPosition []int) ([]int, bool) {
 	if inputMap[currentPosition[0]][currentPosition[1]+1] == "." || inputMap[currentPosition[0]][currentPosition[1]+1] == "x" {
 		newPosition = append(newPosition, currentPosition[0])
 		newPosition = append(newPosition, currentPosition[1]+1)
-	} else if inputMap[currentPosition[0]][currentPosition[1]+1] == "#" {
+	} else if inputMap[currentPosition[0]][currentPosition[1]+1] == "#" || inputMap[currentPosition[0]][currentPosition[1]+1] == "o" {
 		newPosition = append(newPosition, currentPosition[0])
 		newPosition = append(newPosition, currentPosition[1])
 		changeDir = true
@@ -87,7 +87,7 @@ func checkDown(inputMap [][]string, currentPosition []int) ([]int, bool) {
 	if inputMap[currentPosition[0]+1][currentPosition[1]] == "." || inputMap[currentPosition[0]+1][currentPosition[1]] == "x" {
 		newPosition = append(newPosition, currentPosition[0]+1)
 		newPosition = append(newPosition, currentPosition[1])
-	} else if inputMap[currentPosition[0]+1][currentPosition[1]] == "#" {
+	} else if inputMap[currentPosition[0]+1][currentPosition[1]] == "#" || inputMap[currentPosition[0]+1][currentPosition[1]] == "o" {
 		newPosition = append(newPosition, currentPosition[0])
 		newPosition = append(newPosition, currentPosition[1])
 		changeDir = true
@@ -101,7 +101,7 @@ func checkLeft(inputMap [][]string, currentPosition []int) ([]int, bool) {
 	if inputMap[currentPosition[0]][currentPosition[1]-1] == "." || inputMap[currentPosition[0]][currentPosition[1]-1] == "x" {
 		newPosition = append(newPosition, currentPosition[0])
 		newPosition = append(newPosition, currentPosition[1]-1)
-	} else if inputMap[currentPosition[0]][currentPosition[1]-1] == "#" {
+	} else if inputMap[currentPosition[0]][currentPosition[1]-1] == "#" || inputMap[currentPosition[0]][currentPosition[1]-1] == "o" {
 		newPosition = append(newPosition, currentPosition[0])
 		newPosition = append(newPosition, currentPosition[1])
 		changeDir = true
@@ -109,15 +109,16 @@ func checkLeft(inputMap [][]string, currentPosition []int) ([]int, bool) {
 	return newPosition, changeDir
 }
 
-func part1(inputMap [][]string) {
-	var visitedPositions [][]int
+func part1(inputMap [][]string) [][]string {
+	var startPosition []int
 	var currentPosition []int
 	var currentDirection = "Up"
 	for x := 0; x < len(inputMap); x++ {
 		for y := 0; y < len(inputMap[x]); y++ {
 			if inputMap[x][y] == "^" {
+				startPosition = append(currentPosition, x, y)
 				currentPosition = append(currentPosition, x, y)
-				visitedPositions = append(visitedPositions, currentPosition)
+
 				inputMap[x][y] = "x"
 			}
 		}
@@ -134,7 +135,7 @@ func part1(inputMap [][]string) {
 				currentDirection = changeDirection(currentDirection)
 			}
 			currentPosition = currentPos
-			visitedPositions = append(visitedPositions, currentPosition)
+
 			inputMap[currentPosition[0]][currentPosition[1]] = "x"
 
 		} else if currentDirection == "Down" {
@@ -147,7 +148,7 @@ func part1(inputMap [][]string) {
 				currentDirection = changeDirection(currentDirection)
 			}
 			currentPosition = currentPos
-			visitedPositions = append(visitedPositions, currentPosition)
+
 			inputMap[currentPosition[0]][currentPosition[1]] = "x"
 
 		} else if currentDirection == "Left" {
@@ -159,7 +160,7 @@ func part1(inputMap [][]string) {
 				currentDirection = changeDirection(currentDirection)
 			}
 			currentPosition = currentPos
-			visitedPositions = append(visitedPositions, currentPosition)
+
 			inputMap[currentPosition[0]][currentPosition[1]] = "x"
 		} else if currentDirection == "Right" {
 
@@ -171,7 +172,7 @@ func part1(inputMap [][]string) {
 				currentDirection = changeDirection(currentDirection)
 			}
 			currentPosition = currentPos
-			visitedPositions = append(visitedPositions, currentPosition)
+
 			inputMap[currentPosition[0]][currentPosition[1]] = "x"
 		}
 
@@ -186,11 +187,114 @@ func part1(inputMap [][]string) {
 	}
 	fmt.Println("Part1 = ", count)
 
+	inputMap[startPosition[0]][startPosition[1]] = "^"
+	// return input map with all visited locations on
+	return inputMap
+}
+
+// PART 2
+func part2(inputMap [][]string, updatedInputMap [][]string) {
+	// Create array of unique visited locations
+	var uniqueVisitedLocations [][]int
+	for x := 0; x < len(updatedInputMap); x++ {
+		for y := 0; y < len(updatedInputMap[x]); y++ {
+			if updatedInputMap[x][y] == "x" {
+				uniqueVisitedLocations = append(uniqueVisitedLocations, []int{x, y})
+			}
+		}
+	}
+	fmt.Println("uniqueVisitedLocations = ", uniqueVisitedLocations)
+	var loopCount = 0
+	var InfiniteLoopsFound = 0
+	// Loop Over All visited locations and try adding a 0 to each and see if the previous code gets stuck in a loop
+	for i := 0; i < len(uniqueVisitedLocations); i++ {
+		loopCount = 0
+		var workingInputMap [][]string
+		workingInputMap = ReadFile("./input.txt")
+		workingInputMap[uniqueVisitedLocations[i][0]][uniqueVisitedLocations[i][1]] = "o"
+		//workingInputMap[8][3] = "o"
+		var currentPosition []int
+		var currentDirection = "Up"
+		for x := 0; x < len(workingInputMap); x++ {
+			for y := 0; y < len(workingInputMap[x]); y++ {
+				if workingInputMap[x][y] == "^" {
+					currentPosition = append(currentPosition, x, y)
+
+					workingInputMap[x][y] = "x"
+				}
+			}
+		}
+
+		var stillOnMap = true
+		for stillOnMap {
+
+			if currentDirection == "Up" {
+				if currentPosition[0] == 0 {
+					break
+				}
+				currentPos, changeDir := checkUp(workingInputMap, currentPosition)
+				if changeDir {
+					currentDirection = changeDirection(currentDirection)
+				}
+				currentPosition = currentPos
+
+				workingInputMap[currentPosition[0]][currentPosition[1]] = "x"
+
+			} else if currentDirection == "Down" {
+				if currentPosition[0] == len(workingInputMap)-1 {
+					break
+				}
+
+				currentPos, changeDir := checkDown(workingInputMap, currentPosition)
+				if changeDir {
+					currentDirection = changeDirection(currentDirection)
+				}
+				currentPosition = currentPos
+
+				workingInputMap[currentPosition[0]][currentPosition[1]] = "x"
+
+			} else if currentDirection == "Left" {
+				if currentPosition[1] == 0 {
+					break
+				}
+				currentPos, changeDir := checkLeft(workingInputMap, currentPosition)
+				if changeDir {
+					currentDirection = changeDirection(currentDirection)
+				}
+				currentPosition = currentPos
+
+				workingInputMap[currentPosition[0]][currentPosition[1]] = "x"
+			} else if currentDirection == "Right" {
+
+				if currentPosition[1] == len(workingInputMap[0])-1 {
+					break
+				}
+				currentPos, changeDir := checkRight(workingInputMap, currentPosition)
+				if changeDir {
+					currentDirection = changeDirection(currentDirection)
+				}
+				currentPosition = currentPos
+
+				workingInputMap[currentPosition[0]][currentPosition[1]] = "x"
+			}
+			loopCount++
+
+			if loopCount > 10000 {
+				fmt.Println("infiniteLoopFound", loopCount)
+				InfiniteLoopsFound++
+				break
+			}
+		}
+
+	}
+	fmt.Println("InfiniteLoopsFound ", InfiniteLoopsFound)
 }
 
 func main() {
 	fmt.Println("Advent Of Code 2024 - Day 6")
 	var inputMap = ReadFile("./input.txt")
-	part1(inputMap)
+	var updatedInputMap = part1(inputMap)
+	var part2InputMap = ReadFile("./input.txt")
+	part2(part2InputMap, updatedInputMap)
 
 }
