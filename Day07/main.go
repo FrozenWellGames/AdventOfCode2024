0ea571c669308210830a38ffe8cc1c5d4e93816a
FrozenWellGames <frozenwellgames@gmail.com>
2024-12-07 09:24:47 +0000

    Day 7 part 1 set up

--- Day07/main.go
+++ Day07/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
@@ -29,11 +31,47 @@ func ReadFile(filepath string) []string {
 		cnt++
 	}
 	return returnInput
+
+}
+
+func convertStringToInt(s string) int {
+	returnInt, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return 0
+	}
+	return returnInt
+}
+
+func formatData(input []string) ([]int, [][]int) {
+	answerArray := []int{}
+	numbersArray := [][]int{}
+	for i := 0; i < len(input); i++ {
+		firstSplit := strings.Split(input[i], ":")
+		answerArray = append(answerArray, convertStringToInt(firstSplit[0]))
+		trimmedNumbers := strings.TrimSpace(firstSplit[1])
+		secondSplit := strings.Split(trimmedNumbers, " ")
+		numbers := []int{}
+		for i := 0; i < len(secondSplit); i++ {
+			numbers = append(numbers, convertStringToInt(secondSplit[i]))
+		}
+		numbersArray = append(numbersArray, numbers)
+	}
+
+	return answerArray, numbersArray
+}
+
+func part1(answersArray []int, numbersArray [][]int) {
+
 }
 
 func main() {
 	fmt.Println("Advent Of Code 2024 - Day 7")
 	var input = ReadFile("./test.txt")
-	fmt.Println(input[0])
+	fmt.Println(input)
+	answersArray, numbersArray := formatData(input)
+	fmt.Println("answersArray ", answersArray)
+	fmt.Println("numbersArray", numbersArray)
+	part1(answersArray, numbersArray)
 
 }
