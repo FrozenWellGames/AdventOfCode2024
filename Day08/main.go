289342213fbd6e2a99407591b16d3f76b7788b8a
FrozenWellGames <frozenwellgames@gmail.com>
2024-12-08 11:08:23 +0000

    Day 8 Part 1 data formatting

--- Day08/main.go
+++ Day08/main.go
@@ -4,8 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
+	"strings"
 )
+
+type coordinates struct {
+	x int
+	y int
+}
+
+// Set is a collection of unique elements
+type antenna struct {
+	key    string
+	coOrds []coordinates
+}
 
 func check(e error) {
 	if e != nil {
@@ -33,6 +46,15 @@ func ReadFile(filepath string) []string {
 
 }
 
+func indexOf(element string, data []antenna) int {
+	for i := 0; i < len(data); i++ {
+		if data[i].key == element {
+			return i
+		}
+	}
+	return -1
+}
+
 func convertStringToInt(s string) int {
 	returnInt, err := strconv.Atoi(s)
 	if err != nil {
@@ -44,13 +66,51 @@ func convertStringToInt(s string) int {
 
 func convertIntToString(s int) string {
 	returnString := strconv.Itoa(s)
-
 	return returnString
+}
+
+func formatData(input []string) [][]string {
+	returnSlice := [][]string{}
+	for i := 0; i < len(input); i++ {
+		returnSlice = append(returnSlice, strings.Split(input[i], ""))
+	}
+	return returnSlice
+}
+
+func createSets(formattedData [][]string) {
+
+	var uniqueAntennas = []string{}
+	var antennas = []antenna{}
+
+	for x := 0; x < len(formattedData); x++ {
+		for y := 0; y < len(formattedData[x]); y++ {
+			if formattedData[x][y] != "." {
+				if !slices.Contains(uniqueAntennas, formattedData[x][y]) {
+					uniqueAntennas = append(uniqueAntennas, formattedData[x][y])
+
+					var test = antenna{key: formattedData[x][y], coOrds: []coordinates{}}
+					test.coOrds = append(test.coOrds, coordinates{x: x, y: y})
+					antennas = append(antennas, test)
+				} else {
+					localCoOrds := coordinates{x: x, y: y}
+					indexOfAntenna := indexOf(formattedData[x][y], antennas)
+					if indexOfAntenna != -1 {
+						antennas[indexOfAntenna].coOrds = append(antennas[indexOfAntenna].coOrds, localCoOrds)
+					}
+				}
+
+			}
+		}
+	}
+	fmt.Println(antennas)
+
 }
 
 func main() {
 	fmt.Println("Advent Of Code 2024 - Day 8")
-	var input = ReadFile("./input.txt")
-	fmt.Println(input)
+	var input = ReadFile("./test.txt")
+	formattedData := formatData(input)
+	fmt.Println(formattedData)
+	createSets(formattedData)
 
 }
